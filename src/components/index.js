d80d50193cf47efbec781afeba449210089f30c9
RaphaMarinh0 <rapha0512@outlook.com>
2021-02-05 16:53:54 -0300

    commit compo

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Portifolio from './Portifolio';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+
+export {About, Contact, Footer, Header, Portifolio};
